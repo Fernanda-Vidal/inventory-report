3917431b824baf2cdeb17f2dd4e7db7b3843a553
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-04 20:38:09 -0300

    refactor: Simplifica retorno

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,18 +4,16 @@ from collections import Counter
 class SimpleReport:
     @staticmethod
     def generate(list_dict):
-        empresas = []
+        comp = []
 
         oldest = min(dict["data_de_fabricacao"] for dict in list_dict)
-        next_expiration = min(
-            dict["data_de_validade"] for dict in list_dict
-        )
+        next_expiration = min(dict["data_de_validade"] for dict in list_dict)
 
         for d in list_dict:
-            empresas.append(d["nome_da_empresa"])
+            comp.append(d["nome_da_empresa"])
 
-        return f"""
-            Data de fabricação mais antiga: {oldest}
-            Data de validade mais próxima: {next_expiration}
-            Empresa com mais produtos: {Counter(empresas).most_common(1)[0][0]}
-            """
+        return (
+            f"Data de fabricação mais antiga: {oldest}\n"
+            f"Data de validade mais próxima: {next_expiration}\n"
+            f"Empresa com mais produtos: {Counter(comp).most_common(1)[0][0]}"
+            )
