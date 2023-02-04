cabfb73bb4a9ee530d5ee1980653b1ea5145f454
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-04 20:21:02 -0300

    feat: requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,21 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list_dict):
+        empresas = []
+
+        oldest = min(dict["data_de_fabricacao"] for dict in list_dict)
+        next_expiration = min(
+            dict["data_de_validade"] for dict in list_dict
+        )
+
+        for d in list_dict:
+            empresas.append(d["nome_da_empresa"])
+
+        return f"""
+            Data de fabricação mais antiga: {oldest}
+            Data de validade mais próxima: {next_expiration}
+            Empresa com mais produtos: {Counter(empresas).most_common(1)[0][0]}
+            """
---
+++ inventory_report/reports/tempCodeRunnerFile.py
@@ -0,0 +1,30 @@
+
+# lista_dicionario = [
+#     {
+#         "id": 1,
+#         "nome_do_produto": "CADEIRA",
+#         "nome_da_empresa": "Forces of Nature",
+#         "data_de_fabricacao": "2022-04-04",
+#         "data_de_validade": "2023-02-04",
+#         "numero_de_serie": "FR48",
+#         "instrucoes_de_armazenamento": "Conservar em local fresco",
+#     },
+#     {
+#         "id": 2,
+#         "nome_do_produto": "MESA",
+#         "nome_da_empresa": "Forces of Nature",
+#         "data_de_fabricacao": "2021-04-04",
+#         "data_de_validade": "2024-02-09",
+#         "numero_de_serie": "FR48",
+#         "instrucoes_de_armazenamento": "Conservar em local fresco",
+#     },
+#     {
+#         "id": 3,
+#         "nome_do_produto": "SOFA",
+#         "nome_da_empresa": "New Company",
+#         "data_de_fabricacao": "2019-04-04",
+#         "data_de_validade": "2030-02-09",
+#         "numero_de_serie": "FR48",
+#         "instrucoes_de_armazenamento": "Conservar em local fresco",
+#     },
+# ]
